4769af7b2a6a5bb09e6fe1f1d98ba9876f7db3bf
EmmanuelNoel <jahounoel@gmail.com>
2024-06-05 23:02:16 +0100

    Fix index.js

--- index.js
+++ index.js
@@ -31,24 +31,28 @@ var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
+
+
 app.get("/api/:date?", function(req, res) {
-  var reqString = req.params.date;
-  var resDate;
-  if (reqString == undefined) {
+  const date_string = req.params.date;
+  let resDate;
+
+  if (!date_string) {
     resDate = new Date();
   } else {
-    if (!/^\d{4}-/.test(reqString)) reqString = parseInt(reqString);
-    resDate = new Date(reqString);
-    // this comparision is used to see if the date is a valid date, is there another way to do this?
-    if (resDate.getTime() !== resDate.getTime()) {
-      res.json({ error: "Invalid Date" });
+    // Si le paramètre est un nombre, le traiter comme un timestamp Unix
+    if (!isNaN(date_string)) {
+      resDate = new Date(parseInt(date_string));
+    } else {
+      resDate = new Date(date_string);
+    }
+
+    // Vérifier si la date est invalide
+    if (isNaN(resDate.getTime())) {
+      return res.json({ error: "Invalid Date" });
     }
   }
-  res.json({ unix: resDate.valueOf(), utc: resDate.toUTCString() });
-});
 
-app.get("/api/timestamp/", function(req, res) {
-  var resDate = new Date();
   res.json({ unix: resDate.valueOf(), utc: resDate.toUTCString() });
 });
 
